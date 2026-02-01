5586d1e0d2ba7841cf2702a9e8739a1ab4776c5e
AnttiVainikka <antti.vainikka36@gmail.com>
2026-02-01 11:18:05 +0200

    4.10-4.14 done

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,15 +1,69 @@
 const blogsRouter = require("express").Router()
 const Blog = require("../models/blog")
+const logger = require("../utils/logger")
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
     response.json(blogs)
 })
 
+blogsRouter.get("/:id", async (request, response) => {
+    let blog = []
+    try {
+        blog = await Blog.findById(request.params.id)
+    } catch (error) {
+        return response.status(404).end()
+    }
+    
+    if (blog) {
+        response.json(blog)
+    } else {
+        response.status(404).end()
+    }
+})
+
 blogsRouter.post('/', async (request, response) => {
-    const blog = new Blog(request.body)
+    if (!request.body.title || !request.body.url) {
+        return response.status(400).json({error : "Title and/or url is missing"})
+    }
+
+    let blog = new Blog(request.body)
+
+    if (!blog.likes) {
+            blog.likes = 0
+        }
     const savedBlog = await blog.save()
     response.status(201).json(savedBlog)
+})
+
+blogsRouter.delete("/:id", async (request, response) => {
+    try {
+        await Blog.findByIdAndDelete(request.params.id)
+    } catch (error) {
+        logger.error("Invalid id for DELETE request")
+    }
+    response.status(204).end()
+})
+
+blogsRouter.put("/:id", async (request, response) => {
+    const { title, author, url, likes} = request.body
+    let blog = []
+    try {
+        blog = await Blog.findById(request.params.id)
+    } catch (error) {
+        logger.error("Invalid id for PUT request")
+        return response.status(404).end()
+    }
+    if (!blog) {
+        return response.status(404).end()
+    }
+    blog.title = title ? title : blog.title
+    blog.author = author ? author : blog.author
+    blog.url = url ? url : blog.url
+    blog.likes = likes ? likes : blog.likes
+
+    await blog.save()
+    return response.json(blog)
 })
 
 module.exports = blogsRouter
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -33,6 +33,126 @@ describe('GET request to /api/blogs', () => {
     })
 })
 
+describe('POST request to /api/blogs', () => {
+
+    test("POSTing blog increases blog count by one and new blog can be found", async () => {
+        const newBlog = {
+            title: "POST it",
+            author: "POSTER",
+            url: "jospa",
+            likes: 44
+        }
+
+        await api
+            .post("/api/blogs")
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+        
+        const response = await api.get("/api/blogs")
+        const titles = response.body.map(blog=> blog.title)
+        assert.strictEqual(response.body.length, blogs.length + 1)
+        assert(titles.includes("POST it"))
+    })
+
+    test("POSTing blog without given like value defaults likes to 0", async () => {
+        const likelessBlog = {
+            title: "No likes, sad",
+            author: "Eppu Epäsuosittu",
+            url: "lonely"
+        }
+        await api.post("/api/blogs").send(likelessBlog)
+        const response = await api.get("/api/blogs")
+        const addedBlog = response.body.filter((blog) => blog.author == "Eppu Epäsuosittu")[0]
+        assert.strictEqual(addedBlog.likes, 0)
+        })
+    
+    test ("POSTing blog without title or url returns 400 Bad Request and no blog is added", async () => {
+
+        const titlelessBlog = {
+            author: "POSTER",
+            url: "jospa",
+            likes: 44
+        }
+        const urlessBlog = {
+            title: "POST it",
+            author: "POSTER",
+            likes: 44
+        }
+        
+        await api
+            .post("/api/blogs")
+            .send(titlelessBlog)
+            .expect(400)
+        
+        await api
+            .post("/api/blogs")
+            .send(urlessBlog)
+            .expect(400)
+        
+        const response = await api.get("/api/blogs")
+        assert.strictEqual(response.body.length, blogs.length)
+    })
+})
+
+describe('DELETE request to /api/blogs/id', () => {
+
+    test('DELETE request with correct id deletes blog and returns 204', async () => {
+        await api
+            .delete(`/api/blogs/${blogs[0]._id}`)
+            .expect(204)
+
+        const response = await api.get("/api/blogs")
+        assert.strictEqual(response.body.length, blogs.length - 1)
+        const titles = response.body.map(blog=> blog.title)
+        assert(!titles.includes("React patterns"))
+    })
+
+    test("DELETE request with invalid id doesn't crash system and returns 204", async () => {
+        await api
+            .delete(`/api/blogs/asdfasdfsd`)
+            .expect(204)
+    })
+})
+
+describe('PUT request to /api/blogs/id', () => {
+
+    test('PUT request successfully updated blog when all values are given and returns the blog', async () => {
+        const updatedValues = {
+            title: "Python patterns",
+            author: "Cobra Chan",
+            url: "https://pythonpatterns.com/",
+            likes: 76
+        }
+        const updatedBlog = await api.put(`/api/blogs/${blogs[0]._id}`).send(updatedValues)
+        const response = await api.get("/api/blogs")
+        const result = response.body.filter((blog) => blog.id == blogs[0]._id)[0]
+        assert.deepStrictEqual(updatedBlog.body,result)
+        assert.strictEqual("Python patterns",result.title)
+    })
+
+    test('PUT request successfully updated blog when only likes are given', async () => {
+        const updatedLikes = {
+            likes: 67
+        }
+        await api.put(`/api/blogs/${blogs[0]._id}`).send(updatedLikes)
+        const response = await api.get("/api/blogs")
+        const result = response.body.filter((blog) => blog.id == blogs[0]._id)[0]
+        assert.deepStrictEqual(67,result.likes)
+    })
+
+    test("PUT request with invalid or missing id doesn't crash system and returns 404", async () => {
+        await api
+            .put(`/api/blogs/asdfasdfsd`)
+            .send({likes: 67})
+            .expect(404)
+        await api
+            .put(`/api/blogs/5a422a851b54a676534d17f7`)
+            .send({likes: 67})
+            .expect(404)
+    })
+})
+
 after(async () => {
   await mongoose.connection.close()
 })
